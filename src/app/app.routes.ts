2a300ae1b768986594442aa4d2a5e63fc31a2b6a
Ashrita Nayak <ashrita.nayak@gyansys.com>
2026-02-12 13:20:24 +0530

    implement lazy-loaded routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { WorkflowDetail } from './workflows/workflow-detail/workflow-detail';
+import { workflowResolver } from './workflows/workflow.resolver';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+        path: '',
+        redirectTo: 'workflows',
+        pathMatch: 'full'
+    },
+
+    {
+        path: 'workflows',
+        loadComponent: () => import('./workflows/workflow-list/workflow-list').then(m => m.WorkflowList)
+    },
+    {
+        path: 'workflows/:id',
+
+         loadComponent: () => import('./workflows/workflow-detail/workflow-detail')
+         .then(m => m.WorkflowDetail),
+         resolve: {
+            workflow : workflowResolver
+    }
+}
+
+    
+];
