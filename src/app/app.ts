4b46cfd4fa868fa5dd9181e21ca954b0933d7152
Ashrita Nayak <ashrita.nayak@gyansys.com>
2026-02-12 11:52:39 +0530

    update root component to support routing architecture

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,29 +1,14 @@
 import { Component} from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { WorkflowList } from './workflows/workflow-list/workflow-list';
-import { WorkflowService } from './workflows/workflow.service';
-import { Workflow } from './workflows/workflow.model';
-import { Observable } from 'rxjs';
+import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [WorkflowList, CommonModule],
+  imports: [ RouterOutlet ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 
 
-export class App {
-  workflows$!: Observable<Workflow[]>; //This property is an Observable that will hold an array of Workflow objects. The $ suffix is a common convention to indicate that this property is an Observable.//
-  
-  constructor(private workflowService: WorkflowService) {
-    this.workflows$ = this.workflowService.getWorkflows();
-     //In the constructor, the WorkflowService is injected, and the getWorkflows() method is called to assign the Observable of workflows to the workflows$ property.//
-  }
-
- onWorkflowSelected(id: number): void { //This method is called when a workflow is selected in the WorkflowList component. It receives the ID of the selected workflow as a parameter and logs it to the console.//
-    console.log('Selected Workflow ID:', id);
- }
-}
+export class App {}
