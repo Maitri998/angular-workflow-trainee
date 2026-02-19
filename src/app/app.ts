880ba77549c62108c9ec3353b953a5025584787d
Ashrita Nayak <ashrita.nayak@gyansys.com>
2026-02-19 22:31:33 +0530

    Updated root App component to integrate AuthService and implement logout with route redirection.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,23 @@
 import { Component} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ RouterOutlet ],
+  imports: [ RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 
+export class App {
+constructor(public auth: AuthService, private router: Router) {}
 
-export class App {}
+logout() {
+    this.auth.logout();
+    this.router.navigate(['/login']);
+  } 
+  
+}
