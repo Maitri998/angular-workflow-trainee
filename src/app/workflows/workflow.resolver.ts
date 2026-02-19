de45e9cdd77e5a5f073cf99625b02f1a10f74d2d
Ashrita Nayak <ashrita.nayak@gyansys.com>
2026-02-19 21:35:46 +0530

    Implemented route resolver to pre-fetch workflow data before detail view activation.

--- src/app/workflows/workflow.resolver.ts
+++ src/app/workflows/workflow.resolver.ts
@@ -2,18 +2,31 @@ import { inject } from "@angular/core";
 import { ResolveFn } from "@angular/router";
 import { Workflow } from "./workflow.model";
 import { WorkflowService } from "./workflow.service";
-import { map } from "rxjs";
+import { map, tap, first } from "rxjs";
 
 
-
-export const workflowResolver: ResolveFn<Workflow>
+export const workflowResolver: ResolveFn<Workflow | undefined> //this will ensure that the component receives resolved data instead of fetching manually
  = (route) => {
     const service = inject(WorkflowService);
-    const id = Number(route.paramMap.get('id'));
+    const id = Number(route.paramMap.get('id')); // this will extract workflow ID from route parameter and convert to number
+
+    console.log('Resolver searching for ID:', id);
 
     return service.getWorkflows().pipe(
 
-        map(workflows => workflows.find(w => w.id === id)!)
+        first(), //this will take the first value from the observable and automatically stop listening
+
+        map(workflows => workflows.find(w => w.id === id)),
+        tap(result => {
+            if (!result) 
+                {
+                console.error('Resolver did not find workflow with ID:', id);
+                } 
+            else
+                 {
+                console.log('Resolver found workflow:', result);
+                 }
+        })
     );
 }
 
